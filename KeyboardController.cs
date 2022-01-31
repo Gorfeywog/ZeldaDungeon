74bf4dbdca19e77c922979b1b0892aa46d94d1f7
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-01-31 11:53:48 -0500

    Add IController, ICommand interfaces, add KeyboardController class and Quit class, add basic code to Game1 class to make use of these

---
+++ KeyboardController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZeldaDungeon.Commands;
+
+namespace ZeldaDungeon
+{
+    public class KeyboardController : IController
+    {
+        private KeyboardState state;
+        private IDictionary<Keys, ICommand> commands;
+        public KeyboardController()
+        {
+            UpdateState(); // don't let state be null
+            commands = new Dictionary<Keys, ICommand>();
+        }
+        public void UpdateState()
+        {
+            state = Keyboard.GetState();
+        }
+        public void ExecuteCommands()
+        {
+            foreach (Keys k in state.GetPressedKeys())
+            {
+                if (commands.ContainsKey(k))
+                {
+                    commands[k].Execute();
+                }
+            }
+        }
+        public void RegisterCommand(Keys k, ICommand c)
+        {
+            commands[k] = c;
+        }
+
+    }
+}

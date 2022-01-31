463512937a847b4c1e7bff2917308cdc62c70724
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-01-31 11:53:48 -0500

    Add IController, ICommand interfaces, add KeyboardController class and Quit class, add basic code to Game1 class to make use of these. added stub for Reset command.

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using ZeldaDungeon.Commands;
 
 namespace ZeldaDungeon
 {
@@ -8,18 +9,21 @@ namespace ZeldaDungeon
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private KeyboardController keyboardController;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            keyboardController = new KeyboardController();
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            keyboardController.RegisterCommand(Keys.Q, new Quit(this));
+            keyboardController.RegisterCommand(Keys.R, new Reset(this)); // reset is not implemented yet
             base.Initialize();
         }
 
@@ -32,9 +36,8 @@ namespace ZeldaDungeon
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
+            keyboardController.UpdateState();
+            keyboardController.ExecuteCommands();
             // TODO: Add your update logic here
 
             base.Update(gameTime);
---
+++ IController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon
+{
+    public interface IController
+    {
+        public void UpdateState();
+        public void ExecuteCommands();
+    }
+}

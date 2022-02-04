b7bc08ea0218819aed8d5ae950d9b5d7983b2c13
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-02-04 11:32:51 -0500

    add basic BlockSpriteFactory class

---
+++ Sprites/BlockSpriteFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon.Sprites
+{
+    class BlockSpriteFactory
+    {
+        private static BlockSpriteFactory instance = new BlockSpriteFactory();
+        private static BlockSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        private BlockSpriteFactory() { }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+
+        }
+    }
+}

6516171b05adde9b10718c9e432dc954173a2528
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-02-03 21:28:47 -0500

    add interfaces, in skeleton form absent of many methods

---
+++ Entities/IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon.Entities
+{
+    public interface IDrawable
+    {
+        public void Draw();
+        public void UpdateSprite();
+    }
+}
---
+++ Sprites/ISprite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon.Sprites
+{
+    public interface ISprite
+    {
+        public void Draw(); // maybe change the SpriteBatch to be a parameter? unsure if it should be that or a private variable
+        public void Update();
+    }
+}

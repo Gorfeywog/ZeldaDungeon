4a1c942b9b4d47a06adb570730a59c3e4d3093be
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-02-13 14:06:20 -0500

    implement static blocks

---
+++ Sprites/BlockSprites/StaticBlockSprite.cs
@@ -0,0 +1,31 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon.Sprites.BlockSprites
+{
+    class StaticBlockSprite : ISprite
+    {
+        private Texture2D spritesheet;
+        private int width;
+        private int height;
+        private Rectangle sourceRectangle;
+        public StaticBlockSprite(Texture2D spritesheet, int width, int height, Point topLeft)
+        {
+            this.spritesheet = spritesheet;
+            this.width = width;
+            this.height = height;
+            sourceRectangle = new Rectangle(topLeft.X, topLeft.Y, width, height);
+
+        }
+        public void Draw(SpriteBatch spriteBatch, Point topLeft)
+        {
+            Rectangle destinationRectangle = new Rectangle(topLeft.X, topLeft.Y, width, height);
+            spriteBatch.Draw(spritesheet, destinationRectangle, sourceRectangle, Color.White);
+        }
+
+        public void Update() { }
+    }
+}

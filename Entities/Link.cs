94a5a94cfb519ab479eba6e11259f80adbf6493e
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-02-14 15:07:41 -0500

    Change commands to make Link movement less janky, bind control for Link attacks (which barely work), rework LinkStateMachine to use properties instead of fields so animations don't break, added new properties to ILink

--- Entities/Link.cs
+++ Entities/Link.cs
@@ -8,7 +8,8 @@ public class Link : ILink
 {
 	private LinkStateMachine stateMachine;
 	private ISprite linkSprite;
-	private Point position;
+	public Point Position { get; private set; }
+	public Direction Direction { get => stateMachine.CurrentDirection;  }
 
 	public Link()
 	{
@@ -38,30 +39,33 @@ public class Link : ILink
 	public void Update()
 	{
 		stateMachine.Update();
-		linkSprite = stateMachine.LinkSprite(); // TODO - make this not break animations!
+		if (stateMachine.HasNewSprite)
+		{
+			linkSprite = stateMachine.LinkSprite(); // only get a new sprite if we need to
+		}
 		linkSprite.Update();
-		if (stateMachine.currentState == LinkStateMachine.LinkState.Walking)
+		if (stateMachine.CurrentState == LinkStateMachine.LinkState.Walking)
         {
 			// move forward
 			int speed = 1; // TODO - handle speed better
-			Point newPos = stateMachine.currentDirection switch
+			Point newPos = stateMachine.CurrentDirection switch
 			{
-				Direction.Up => new Point(position.X, position.Y - speed),
-				Direction.Down => new Point(position.X, position.Y + speed),
-				Direction.Left => new Point(position.X - speed, position.Y),
-				Direction.Right => new Point(position.X + speed, position.Y),
+				Direction.Up => new Point(Position.X, Position.Y - speed),
+				Direction.Down => new Point(Position.X, Position.Y + speed),
+				Direction.Left => new Point(Position.X - speed, Position.Y),
+				Direction.Right => new Point(Position.X + speed, Position.Y),
 				_ => throw new ArgumentException() // bad direction!
 			};
-			position = newPos;
+			Position = newPos;
         }
 	}
 
 	public void Draw(SpriteBatch spriteBatch)
     {
-		linkSprite.Draw(spriteBatch, position); 
+		linkSprite.Draw(spriteBatch, Position); 
 		// this is an issue since some sprites have the top left somewhere other than Link's top left,
-		// in particular the sword swing left. Best solution is probably to add code in such sprite classes, but
-		// that will be easier to approach once we have a little more to the game
+		// in particular the sword swing left. Best solution is probably to replace those with
+		// overlapped sprites, or something to that effect.
     }
 
     public void StartWalking()
--- Entities/LinkStateMachine.cs
+++ Entities/LinkStateMachine.cs
@@ -5,39 +5,73 @@ using ZeldaDungeon.Sprites;
 public class LinkStateMachine
 {
 	// public enum LinkDirection { Left, Right, Up, Down }; // MOVED TO ZeldaDungeon.Entities.Direction
-	public Direction currentDirection = Direction.Right;
 	public enum LinkState { UsingItem, Damaged, Walking, Idle, Attacking };
 	// should it be possible to have a Damaged, Walking link? maybe change Damaged to a boolean?
-	public LinkState currentState = LinkState.Idle;
+	// would complicate spriting a bit, but it would not be too hard to handle that from a sprite side
+	private Direction currentDirection;
+	public Direction CurrentDirection
+    {
+		get
+        {
+			return currentDirection;
+        }
+		private set
+		{
+			HasNewSprite = true;
+			currentDirection = value;
+        }
+    }
+	private LinkState currentState;
+	public LinkState CurrentState
+    {
+		get
+        {
+			return currentState;
+        }
+		private set
+        {
+			HasNewSprite = true; // new state usually means new sprite
+			currentState = value;
+        }
+    }
+	public bool HasNewSprite { get; private set; } // used to avoid generating new sprite every frame
+
+
+	public LinkStateMachine()
+    {
+		CurrentState = LinkState.Idle;
+		currentDirection = Direction.Right;
+		HasNewSprite = true;
+    }
 
 	public void ChangeDirection(Direction newDirection)
 	{
-		currentDirection = newDirection;
+		CurrentDirection = newDirection;
 	}
 
 	public void UseItem() 
 	{
-		if (currentState != LinkState.UsingItem) currentState = LinkState.UsingItem;
+		if (CurrentState != LinkState.UsingItem) CurrentState = LinkState.UsingItem;
 	}
 
 	public void Attack()
 	{
-		if (currentState != LinkState.Attacking) currentState = LinkState.Attacking;
+		if (CurrentState != LinkState.Attacking) CurrentState = LinkState.Attacking;
 	}
 
 	public void TakeDamage()
 	{
-		if (currentState != LinkState.Damaged) currentState = LinkState.Damaged;
+		if (CurrentState != LinkState.Damaged) CurrentState = LinkState.Damaged;
 	} 
 
 	public void Idle()
     {
-		currentState = LinkState.Idle; // TODO - shouldn't there be more logic here?
+		CurrentState = LinkState.Idle; // TODO - shouldn't there be more logic here?
     }
 
 	public void Walking()
     {
-		currentState = LinkState.Walking; // TODO - shouldn't there be more logic here?
+		CurrentState = LinkState.Walking; // TODO - shouldn't there be more logic here?
 	}
 
 
@@ -47,11 +81,12 @@ public class LinkStateMachine
     }
 	public ISprite LinkSprite()
     {
+		HasNewSprite = false; // just generated a sprite; it must be up to date!
 		LinkSpriteFactory fac = LinkSpriteFactory.Instance;
-		switch (currentState)
+		switch (CurrentState)
         {
 			case LinkState.Idle:
-				return currentDirection switch
+				return CurrentDirection switch
 					{
 						Direction.Up => fac.CreateIdleUpLink(),
 						Direction.Down => fac.CreateIdleDownLink(),
@@ -60,7 +95,7 @@ public class LinkStateMachine
 						_ => throw new ArgumentOutOfRangeException() // if not a valid direction - throw an exception!
 					};
 			case LinkState.Walking:
-				return currentDirection switch
+				return CurrentDirection switch
 				{
 					Direction.Up => fac.CreateWalkingUpLink(),
 					Direction.Down => fac.CreateWalkingDownLink(),
@@ -69,7 +104,7 @@ public class LinkStateMachine
 					_ => throw new ArgumentOutOfRangeException()
 				};
 			case LinkState.Attacking:
-				return currentDirection switch
+				return CurrentDirection switch
 				{
 					Direction.Up => fac.CreateAttackingUpLink(),
 					Direction.Down => fac.CreateAttackingDownLink(),
@@ -78,7 +113,7 @@ public class LinkStateMachine
 					_ => throw new ArgumentOutOfRangeException()
 				};
 			case LinkState.UsingItem:
-				return currentDirection switch
+				return CurrentDirection switch
 				{
 					Direction.Up => fac.CreateUIUpLink(),
 					Direction.Down => fac.CreateUIDownLink(),
@@ -87,7 +122,7 @@ public class LinkStateMachine
 					_ => throw new ArgumentOutOfRangeException()
 				};
 			case LinkState.Damaged:
-				return currentDirection switch
+				return CurrentDirection switch
 				{
 					Direction.Up => fac.CreateDamagedUpLink(),
 					Direction.Down => fac.CreateDamagedDownLink(),

3eb85946bc975d18a0fdd7a450921082e5fa9ecc
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-02-14 21:29:12 -0500

    more dynamic damaged state sprites, state logic reworked a bit

--- Sprites/LinkSpriteFactory.cs
+++ Sprites/LinkSpriteFactory.cs
@@ -31,110 +31,110 @@ namespace ZeldaDungeon.Sprites
         {
             linkSpriteSheet = content.Load<Texture2D>("linksprites");
         }
-        // up, down, left, right
-        public ISprite CreateAttackingLeftLink()
+        // note that these 4 are currently unused; they may return at some point?
+        public ISprite CreateAttackingLeftLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 27, 16, GridToPoint(2,4));
+            return new StaticLinkSprite(linkSpriteSheet, 27, 16, GridToPoint(2,4), damaged);
         }
 
-        public ISprite CreateAttackingRightLink()
+        public ISprite CreateAttackingRightLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 27, 16, GridToPoint(3,4));
+            return new StaticLinkSprite(linkSpriteSheet, 27, 16, GridToPoint(3,4), damaged);
         }
 
-        public ISprite CreateAttackingUpLink()
+        public ISprite CreateAttackingUpLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 27, GridToPoint(0,4));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 27, GridToPoint(0,4), damaged);
         }
 
-        public ISprite CreateAttackingDownLink()
+        public ISprite CreateAttackingDownLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 27, GridToPoint(1,4));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 27, GridToPoint(1,4), damaged);
         }
         // Note that Idle sprites just use the first frame of the
         // walk animation.
         // This should be changed if possible.
-        public ISprite CreateIdleLeftLink()
+        public ISprite CreateIdleLeftLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 2));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 2), damaged);
         }
 
-        public ISprite CreateIdleRightLink()
+        public ISprite CreateIdleRightLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 3));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 3), damaged);
         }
 
-        public ISprite CreateIdleUpLink()
+        public ISprite CreateIdleUpLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 0));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 0), damaged);
         }
 
-        public ISprite CreateIdleDownLink()
+        public ISprite CreateIdleDownLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 1));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 1), damaged);
         }
 
-        public ISprite CreateUILeftLink()
+        public ISprite CreateUILeftLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(3, 5));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(3, 5), damaged);
         }
 
-        public ISprite CreateUIRightLink()
+        public ISprite CreateUIRightLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(4, 5));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(4, 5), damaged);
         }
 
-        public ISprite CreateUIUpLink()
+        public ISprite CreateUIUpLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(5, 5));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(5, 5), damaged);
         }
 
-        public ISprite CreateUIDownLink()
+        public ISprite CreateUIDownLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(2, 5));
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(2, 5), damaged);
+        }
+        /*
+        public ISprite CreateDamagedLeftLink(bool damaged = false)
+        {
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 2), true);
+        }
+        public ISprite CreateDamagedRightLink(bool damaged = false)
+        {
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 3), true);
         }
 
-        public ISprite CreateDamagedLeftLink()
+        public ISprite CreateDamagedUpLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 2), Color.Red);
-        }
-        public ISprite CreateDamagedRightLink()
-        {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 3), Color.Red);
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 0), true);
         }
 
-        public ISprite CreateDamagedUpLink()
+        public ISprite CreateDamagedDownLink(bool damaged = false)
         {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 0), Color.Red);
+            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 1), true);
         }
-
-        public ISprite CreateDamagedDownLink()
-        {
-            return new StaticLinkSprite(linkSpriteSheet, 16, 16, GridToPoint(0, 1), Color.Red);
-        }
-
-        public ISprite CreateWalkingLeftLink()
+        */ // obsoleted; Damaged is a modifier now
+        public ISprite CreateWalkingLeftLink(bool damaged = false)
         {
             Point[] topLefts = { GridToPoint(0, 2), GridToPoint(1, 2) };
-            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts);
+            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts, damaged);
         }
 
-        public ISprite CreateWalkingRightLink()
+        public ISprite CreateWalkingRightLink(bool damaged = false)
         {
             Point[] topLefts = { GridToPoint(0, 3), GridToPoint(1, 3) };
-            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts);
+            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts, damaged);
         }
 
-        public ISprite CreateWalkingUpLink()
+        public ISprite CreateWalkingUpLink(bool damaged = false)
         {
             Point[] topLefts = { GridToPoint(0, 0), GridToPoint(1, 0) };
-            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts);
+            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts, damaged);
         }
 
-        public ISprite CreateWalkingDownLink()
+        public ISprite CreateWalkingDownLink(bool damaged = false)
         {
             Point[] topLefts = { GridToPoint(0, 1), GridToPoint(1, 1) };
-            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts);
+            return new AnimatedLinkSprite(linkSpriteSheet, 16, 16, topLefts, damaged);
         }
 
         private static Point GridToPoint(int x, int y) // convert grid position to position in pixels
--- Sprites/LinkSprites/StaticLinkSprite.cs
+++ Sprites/LinkSprites/StaticLinkSprite.cs
@@ -11,31 +11,48 @@ namespace ZeldaDungeon.Sprites.LinkSprites
         private Texture2D spritesheet;
         private int width;
         private int height;
-        private Color color;
+        private bool damage;
         private Rectangle sourceRectangle;
-        public StaticLinkSprite(Texture2D spritesheet, int width, int height, Point topLeft)
-        {
-            InitializeConstructor(spritesheet, width, height, topLeft, Color.White);
-        }
-        public StaticLinkSprite(Texture2D spritesheet, int width, int height, Point topLeft, Color color)
-        {
-            InitializeConstructor(spritesheet, width, height, topLeft, color);
-        }
-
-        private void InitializeConstructor(Texture2D spritesheet, int width, int height, Point topLeft, Color color)
+        private static readonly Color[] damageColors = { Color.Red, Color.White };
+        private static readonly int damageRepeatDelay = 5;
+        public StaticLinkSprite(Texture2D spritesheet, int width, int height, Point topLeft) : this(spritesheet, width, height, topLeft, false) { } // chained to the other one. use default arg instead maybe?
+        public StaticLinkSprite(Texture2D spritesheet, int width, int height, Point topLeft, bool damage)
         {
             this.spritesheet = spritesheet;
             this.width = width;
             this.height = height;
-            this.color = color;
+            this.damage = damage;
             sourceRectangle = new Rectangle(topLeft.X, topLeft.Y, width, height);
         }
+
         public void Draw(SpriteBatch spriteBatch, Point topLeft)
         {
             Rectangle destinationRectangle = new Rectangle(topLeft.X, topLeft.Y, width, height);
-            spriteBatch.Draw(spritesheet, destinationRectangle, sourceRectangle, color);
+            Color currentColor;
+            if (damage)
+            {
+                currentColor = damageColors[damageColorIndex];
+            }
+            else
+            {
+                currentColor = Color.White;
+            }
+            spriteBatch.Draw(spritesheet, destinationRectangle, sourceRectangle, currentColor);
         }
 
-        public void Update() { } // no animation, no update
+        private int damageColorTimer = damageRepeatDelay;
+        private int damageColorIndex = 0;
+        public void Update()
+        {
+            if (damage)
+            {
+                damageColorTimer--;
+                if (damageColorTimer == 0)
+                {
+                    damageColorIndex = (damageColorIndex + 1) % damageColors.Length;
+                    damageColorTimer = damageRepeatDelay;
+                }
+            }
+        }
     }
 }

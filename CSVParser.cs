f6226dc91b56759e965b2ccd101fe2c1e261a552
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-02-24 13:36:22 -0500

    wrote a shitty csv parser

---
+++ CSVParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon
+{
+    public class CSVParser
+    {
+        private const int width = 16;
+        private const int height = 11;
+        public CSVParser() { }
+        // array corresponds to the room's grid, list stores every entity on a tile?
+        // all rows and columns must have the prescribed dimensions, or bad stuff happens
+        public IList<String>[,] parseFile(string path)
+        {
+            IList<String>[,] tokens = new IList<String>[width,height];
+            string[] lines = System.IO.File.ReadAllLines(path);
+            for (int i = 0; i < height; i++) // height *should* match lines.Length
+            {
+                string[] lineBlocks = lines[i].Split(',');
+                for (int j = 0; j < width; j++)
+                {
+                    string currentBlock = lineBlocks[j];
+                    tokens[j, i] = new List<string>(currentBlock.Split(';'));
+                }
+            }
+            return tokens;
+        }
+    }
+}

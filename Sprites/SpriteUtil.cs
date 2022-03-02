61be1fa9200933bd4ff494e30b5ae6b2d672d4c7
cbooch <booch2nd@gmail.com>
2022-03-02 13:33:47 -0500

    Created an enumeration for each sprite size in the util class, as well as updated most of the factory classes to implement these enumerations

--- Sprites/SpriteUtil.cs
+++ Sprites/SpriteUtil.cs
@@ -8,8 +8,70 @@ namespace ZeldaDungeon.Sprites
     class SpriteUtil
     {
         public static readonly int SCALE_FACTOR = 2; // Scale factor of destination rectangles 
-
         public static readonly int WAIT_TIME = 8; // how many Updates to wait between cycling frame
+
+        //Global random number generator for use in all classes
+        public static Random Rand = new Random();
+
+        //Length is correspondent to height, it is named length because items can
+        //occasionally be used while in different directions, so X and Y would be confusing for these items
+        public enum SpriteSize
+        {
+            GenericBlockX = 16,
+            GenericBlockY = 16,
+            AquamentusX = 24,
+            AquamentusY = 32,
+            KeeseX = 16,
+            KeeseY = 10,
+            FireballX = 8,
+            FireballY = 10,
+            GelX = 8,
+            GelY = 9,
+            RopeX = 15,
+            RopeY = 15,
+            StalfosX = 16,
+            StalfosY = 16,
+            TrapX = 16,
+            TrapY = 16,
+            GoriyaX = 14,
+            GoriyaY = 16,
+            BoomerangX = 5,
+            BoomerangY = 8,
+            WallMasterX = 16,
+            WallMasterY = 16,
+            CloudX = 16,
+            CloudY = 16,
+            HitEffectX = 8,
+            HitEffectY = 8,
+            BombWidth = 8,
+            BombLength = 14,
+            BowWidth = 8,
+            BowLength = 16,
+            ClockWidth = 11,
+            ClockLength = 16,
+            CompassWidth = 11,
+            CompassLength = 12,
+            FairyWidth = 8,
+            FairyLength = 16,
+            HeartContainerWidth = 13,
+            HeartContainerLength = 13,
+            HeartWidth = 7,
+            HeartLength = 8,
+            KeyWidth = 8,
+            KeyLength = 16,
+            MapWidth = 8,
+            MapLength = 16,
+            RupyWidth = 8,
+            RupyLength = 16,
+            TriforceWidth = 10,
+            TriforceLength = 10,
+            ArrowWidth = 5,
+            ArrowLength = 16,
+            SwordWidth = 8,
+            SwordLength = 16,
+            CandleWidth = 8,
+            CandleLength = 16
+        }
 
         private static readonly int gridX = 32;
         private static readonly int gridY = 32;

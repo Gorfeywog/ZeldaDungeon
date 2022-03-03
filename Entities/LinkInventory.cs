5f4249d1ad6f447af3f81e6396d551fadd20742f
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-03-02 23:09:03 -0500

    added a class to represent Link's inventory, keep track of item counts it is pretty jank this branch's name is not a great descriptor anymore

---
+++ Entities/LinkInventory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Text;
+
+namespace ZeldaDungeon.Entities
+{
+    public class LinkInventory
+    {
+        private IDictionary<string, int> itemCount; // maps item names to counts. using names is kinda weird, but IItems don't really work for this either?
+                                                    // all values must be positive, we just remove entries if count drops to 0.
+
+        public LinkInventory()
+        {
+            itemCount = new Dictionary<string, int>();
+        }
+
+        // returns true and decrements count if we can consume the item, if count was zero returns false
+        public bool UseItem(string itemName)
+        {
+            if (itemCount.ContainsKey(itemName))
+            {
+                int ct = itemCount[itemName];
+                itemCount[itemName] = ct - 1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void PickupItem(string itemName)
+        {
+            int ct = itemCount.ContainsKey(itemName) ? itemCount[itemName] : 0;
+            itemCount[itemName] = ct + 1;
+        }
+        public bool HasItem(string itemName)
+        {
+            return itemCount.ContainsKey(itemName);
+        }
+    }
+}

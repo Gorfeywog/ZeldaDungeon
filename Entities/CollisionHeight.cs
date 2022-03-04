9f91818b7190b2554650d0db7b1f375a5c37e707
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-03-04 17:24:27 -0500

    started implementing CollisionHeight

---
+++ Entities/CollisionHeight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon.Entities
+{
+    // entities pass over blocks of lower collision levels, but collide with ones of the same or higher collision level
+    // e.g. floor blocks are level 0 and pushable blocks are level 1, so link (level 1) walks over floors but not pushable blocks
+    // whereas a keese (level 2) flies over both of those, but not through blocks of level 2
+    public enum CollisionHeight
+    {
+        Floor = 0,
+        Normal = 1,
+        High = 2
+    }
+}

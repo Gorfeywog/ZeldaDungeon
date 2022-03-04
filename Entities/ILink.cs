e8a00ce5d20e72b4c60e5f177936b0e18d719d7b
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-03-04 08:41:29 -0500

    add more bugs

--- Entities/ILink.cs
+++ Entities/ILink.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ZeldaDungeon.InventoryItems;
 
 namespace ZeldaDungeon.Entities
 {
@@ -9,13 +10,17 @@ namespace ZeldaDungeon.Entities
     {
         public Point Center { get; }
         public Direction Direction { get; }
-        public List<IEntity> entityList { get; set; }
-
         public void ChangeDirection(Direction nextDirection);
         public void StartWalking();
         public void StopWalking();
         public void TakeDamage();
+        public void PickUp(IPickup pickup);
+        public bool CanPickUp();
+        // could eliminate AddItem and HasItem if we exposed the inventory, but that would maybe
+        // be bad for coupling?
+        public void AddItem(IItem item);
         public void UseItem(IItem item);
+        public bool HasItem(IItem item);
         public void Attack();
     }
 }

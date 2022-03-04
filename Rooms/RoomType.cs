39e0c1480f9351d036a89436b5ffb61a65e6b397
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-03-04 15:48:23 -0500

    add the bow item room, but no way to get to it yet

---
+++ Rooms/RoomType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaDungeon.Rooms
+{
+    // having only 2 types in an enum is kinda weird, but feels more extensible than a bool
+    // numbers are directly associated for easier parsing from csv files, not just arbitrarily!
+    public enum RoomType
+    {
+        Normal = 0,
+        Ladder = 1 
+    }
+}

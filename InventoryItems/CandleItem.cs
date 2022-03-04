07bc9753b590fb0e4420cc719c922eb40297b686
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-03-04 08:32:21 -0500

    readd inventory items (i am good at merging)

---
+++ InventoryItems/CandleItem.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZeldaDungeon.Sprites;
+using ZeldaDungeon.Entities.Projectiles;
+using ZeldaDungeon.Entities;
+
+namespace ZeldaDungeon.InventoryItems
+{
+    public class CandleItem : IItem
+	{
+        public bool Consumable { get => false; }
+        private Game1 g;
+        private bool isRed; // red ones can be used more than once per room
+        public CandleItem(Game1 g, bool isRed)
+        {
+            this.g = g;
+            this.isRed = isRed;
+        }
+        private static int offset = 32;
+        public void UseOn(ILink player)
+        {
+            // uses player.CurrentLoc.Location rather than player.Center since is about the size of Link
+            Point loc = EntityUtils.Offset(player.CurrentLoc.Location, player.Direction, offset);
+            IProjectile proj = new CandleFire(loc, player.Direction);
+            g.RegisterProjectile(proj);
+        }
+
+        public bool CanUseOn(ILink player) => true; // check for using blue candle only once per room?
+        public bool Equals(IItem other)
+        {
+            if (other is CandleItem otherCandle)
+            {
+                return this.g == otherCandle.g && (this.isRed == otherCandle.isRed);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return "candle".GetHashCode() ^ g.GetHashCode() ^ isRed.GetHashCode();
+        }
+    }
+}
+

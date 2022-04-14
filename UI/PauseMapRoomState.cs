d19ceff3549e0cd330302893728e416d887d6b38
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-04-14 11:55:20 -0400

    add a room state class for pause map

---
+++ UI/PauseMapRoomState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ZeldaDungeon.Entities;
+using ZeldaDungeon.Rooms;
+using ZeldaDungeon.Sprites;
+
+namespace ZeldaDungeon.UI
+{
+    class PauseMapRoomState
+    {
+        public bool RoomKnown { get; private set; }
+        private ICollection<Direction> doorDirections; 
+        public PauseMapRoomState(bool roomKnown, ICollection<Direction> doorDirections)
+        {
+            RoomKnown = roomKnown;
+            this.doorDirections = new HashSet<Direction>(doorDirections);
+        }
+        public bool HasDoor(Direction d) => doorDirections.Contains(d);
+    }
+}

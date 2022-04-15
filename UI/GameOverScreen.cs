4728fe9493d985bd9977de218137b30a290096c7
Luke Van De Weghe <vandeweghe.4@osu.edu>
2022-04-14 22:22:45 -0400

    adjust the game over message

--- UI/GameOverScreen.cs
+++ UI/GameOverScreen.cs
@@ -22,7 +22,7 @@ namespace ZeldaDungeon.UI
             int scaledRoomWidth = SpriteUtil.SCALE_FACTOR * SpriteUtil.ROOM_WIDTH;
             Vector2 topLeftVec = topLeft.ToVector2();
             Vector2 center = topLeftVec + new Vector2(scaledRoomWidth / 2.0f, scaledRoomHeight / 2.0f);
-            spriteBatch.DrawString(font, "YOU LOSE", center, Color.Red);
+            spriteBatch.DrawString(font, "YOU LOSE.\nPRESS R TO RESTART.", center, Color.Red);
         }
     }
 }
